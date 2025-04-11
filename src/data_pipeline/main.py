02d1885d9f90a2cf2d63c696c41eb1e199c7168f
Clement Dethoor <clement.dethoor@yahoo.fr>
2025-04-11 13:19:08 -0400

    Refactoring to decouple

--- src/data_pipeline/main.py
+++ src/data_pipeline/main.py
@@ -31,33 +31,25 @@ def fetch_from_endpoint(endpoint : str,output_file : str):
     while True :
         success = False
 
-        url = f"{BASE_URL}/{endpoint}?page={page}&size={SIZE}"
-
         for attempt in range(0,MAX_RETRIES):
             try:
-                response = requests.get(url, timeout=5)
-                response.raise_for_status() # Raise exeception if status > 400, skip next lines and directly go in except block
-                data = response.json()
-
-                items = data.get("items", []) # check if there is still data 
+                items = call_api_page(endpoint, page, SIZE)
                 if not items:
                     print(f"[INFO] Fin de la pagination à la page {page}")
                     return
-                
-                transformed_data = transform_endpoint(endpoint, items)
-                write(transformed_data, output_file)
+                process_page(endpoint, items, output_file)
                 success = True
 
                 if page == 1:
                     return
                 break
 
-            except requests.exceptions.HTTPError as e: 
-                print(f"[ERROR] HTTP {response.status_code} - {e}")
+            except requests.exceptions.HTTPError as e:
+                print(f"[HTTP ERROR] page {page} : {e}")
             except requests.exceptions.Timeout:
-                print(f"[ERROR] Timeout sur la page {page}")
+                print(f"[TIMEOUT] page {page}")
             except requests.exceptions.RequestException as e:
-                print(f"[ERROR] Requête échouée : {e}")
+                print(f"[REQUEST ERROR] page {page} : {e}")
             
             if attempt < MAX_RETRIES:
                 print(f"[WAIT] Attente {WAIT_SECONDS}s avant retry...")
@@ -69,6 +61,16 @@ def fetch_from_endpoint(endpoint : str,output_file : str):
 
         page += 1
 
+def call_api_page(endpoint: str, page: int, size: int) -> list:
+    url = f"{BASE_URL}/{endpoint}?page={page}&size={size}"
+    response = requests.get(url, timeout=5)
+    response.raise_for_status()
+    data = response.json()
+    return data.get("items", [])
+
+def process_page(endpoint: str, items: list, output_file: str):
+    transformed_data = transform_endpoint(endpoint, items)
+    write(transformed_data, output_file)
 
 def transform_endpoint(endpoint : str, data : list) -> list:
     """
