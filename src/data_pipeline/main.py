f7cde2a2b10b505bb6d421eb72de5b59b9b5b18a
Clement Dethoor <clement.dethoor@yahoo.fr>
2025-04-11 13:38:43 -0400

    decouple with factorization

--- src/data_pipeline/main.py
+++ src/data_pipeline/main.py
@@ -7,7 +7,7 @@ import os
 
 BASE_PORT = 8000
 BASE_URL = f"http://localhost:{BASE_PORT}/"
-SIZE = 1
+SIZE = 90
 ENDPOINTS = [
     "tracks",
     "users",
@@ -30,43 +30,42 @@ def fetch_from_endpoint(endpoint : str,output_file : str):
 
     while True :
         success = False
+        items = []
 
         for attempt in range(0,MAX_RETRIES):
-            try:
-                items = call_api_page(endpoint, page, SIZE)
-                if not items:
-                    print(f"[INFO] Fin de la pagination à la page {page}")
-                    return
-                process_page(endpoint, items, output_file)
-                success = True
-
-                if page == 1:
-                    return
+            success, items = call_api_page(endpoint, page, SIZE)
+            if success:
                 break
-
-            except requests.exceptions.HTTPError as e:
-                print(f"[HTTP ERROR] page {page} : {e}")
-            except requests.exceptions.Timeout:
-                print(f"[TIMEOUT] page {page}")
-            except requests.exceptions.RequestException as e:
-                print(f"[REQUEST ERROR] page {page} : {e}")
-            
-            if attempt < MAX_RETRIES:
-                print(f"[WAIT] Attente {WAIT_SECONDS}s avant retry...")
-                time.sleep(WAIT_SECONDS)
+            time.sleep(WAIT_SECONDS)
 
         if not success:
-            print("[FAIL] Échec définitif, arrêt.")
-            break
+            print(f"[FAIL] Échec définitif à la page {page}")
+            return
+
+        transformed_data = transform_endpoint(endpoint, items)
+        write(transformed_data, output_file)
+
+        if len(items) < SIZE:
+            print("[INFO] Fin de la pagination.")
+            return
 
         page += 1
 
-def call_api_page(endpoint: str, page: int, size: int) -> list:
+def call_api_page(endpoint: str, page: int, size: int) -> tuple[bool, list]:
     url = f"{BASE_URL}/{endpoint}?page={page}&size={size}"
-    response = requests.get(url, timeout=5)
-    response.raise_for_status()
-    data = response.json()
-    return data.get("items", [])
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+        items = data.get("items", [])
+        return True, items
+    except requests.exceptions.HTTPError as e:
+        print(f"[HTTP ERROR] page {page} : {e}")
+    except requests.exceptions.Timeout:
+        print(f"[TIMEOUT] page {page}")
+    except requests.exceptions.RequestException as e:
+        print(f"[REQUEST ERROR] page {page} : {e}")
+    return False, []
 
 def process_page(endpoint: str, items: list, output_file: str):
     transformed_data = transform_endpoint(endpoint, items)
