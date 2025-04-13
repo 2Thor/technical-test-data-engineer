6b6d51a53475cdd80f55327dcf30bb8e49a4ebe9
Clement Dethoor <clement.dethoor@yahoo.fr>
2025-04-13 16:52:25 -0400

    Update test to remove errors & clean requirements.txt

---
+++ conftest.py
@@ -0,0 +1,2 @@
+import os
+os.environ["PREFECT_TEST_MODE"] = "0"
--- test/test_data_pipeline.py
+++ test/test_data_pipeline.py
@@ -1,6 +1,7 @@
 from unittest.mock import mock_open, patch,Mock
 from requests.exceptions import HTTPError
 from prefect.logging import disable_run_logger
+from prefect.testing.utilities import prefect_test_harness
 from src.data_pipeline.main import call_api_page,write_to_jsonl,get_output_filename
 from src.data_pipeline.transform import main
 from datetime import datetime
@@ -20,7 +21,7 @@ def test_call_api_page_success(mock_get):
     mock_response.json.return_value = {"items": [{"id": 1}]}
     mock_get.return_value = mock_response
 
-    with disable_run_logger():
+    with prefect_test_harness(), disable_run_logger():
         items = call_api_page.fn("tracks", page=1, size=10)
     assert items == [{"id": 1}]
 
@@ -30,7 +31,7 @@ def test_call_api_page_http_error(mock_get):
     mock_response.raise_for_status.side_effect = HTTPError("500 Internal Server Error")
     mock_get.return_value = mock_response
     with pytest.raises(HTTPError):
-        with disable_run_logger():
+        with prefect_test_harness(), disable_run_logger():
             call_api_page.fn("tracks", page=1, size=10)
     
     assert mock_get.call_count == 1
@@ -62,13 +63,15 @@ def test_transform_data_listen_history():
             "updated_at": "2024-01-02T00:00:00Z"
         }
     ]
-    result = main("listen_history", data)
+    with prefect_test_harness():
+        result = main("listen_history", data)
 
     assert result == expected
 
 def test_get_output_filename():
     base_name = "test_file"
-    file_name = get_output_filename(base_name)
+    with prefect_test_harness():
+        file_name = get_output_filename(base_name)
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
 
     assert file_name == f"{base_name}_{timestamp}.jsonl"
@@ -76,7 +79,7 @@ def test_get_output_filename():
 
 @patch("builtins.open", new_callable=mock_open)
 def test_write_opens_file_and_writes_lines(mock_open, sample_data):
-    with disable_run_logger():
+    with prefect_test_harness(), disable_run_logger():
         write_to_jsonl(sample_data, "test.jsonl")
     
     mock_open.assert_called_once_with("data/test.jsonl", "a", encoding="utf-8")
